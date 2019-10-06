c3c718c792dd87299be023e0e3be8dc818b8c52e
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-10-06 11:09:02 +0330

    " to ' and async jquery ajax

--- public/js/main.tv.js
+++ public/js/main.tv.js
@@ -4,59 +4,49 @@ var chart;
 var japi = {};
 var config = {
 	exchanges: [
-		{ value: "", name: "All Exchanges", desc: "" }
+		{ value: '', name: 'All Exchanges', desc: '' }
 	],
 	symbolsTypes: [
-		{name: "All types", value: ""},
-		{name: "Stock", value: "stock"},
-		{name: "Index", value: "index"}
+		{name: 'All types', value: ''},
+		{name: 'Stock', value: 'stock'},
+		{name: 'Index', value: 'index'}
 	],
-	supportedResolutions: [ "1D" ],
+	supportedResolutions: [ '1D' ],
 	supports_marks: true
 };
 
 // essential:
 japi.onReady = function (callback) {
-	// console.log("onReady()");
+	// console.log('onReady()');
 	
 	setTimeout(callback, 0, config);
 };
 
 japi.resolveSymbol = function (symbolName, onSymbolResolvedCallback, onResolveErrorCallback) {
-	// console.log("resolveSymbol()");
+	console.log('resolveSymbol()');
 	
 	var symbolInfo = {
 		name: symbolName,
 		ticker: symbolName,
-		description: "zob ahan esfahan",
-		session: "0900-1230",
-		timezone: "Asia/Tehran" ,
+		description: 'zob ahan esfahan',
+		session: '0900-1230',
+		timezone: 'Asia/Tehran' ,
 		minmov: 1,
 		pricescale: 1
 	};
 	setTimeout(onSymbolResolvedCallback, 0, symbolInfo);
 };
 
-japi.getBars = function (symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
-	// console.log("getBars()");
+japi.getBars = async function (symbolInfo, resolution, from, to, onHistoryCallback, onErrorCallback, firstDataRequest) {
+	console.log('getBars()');
+	const bars = await $.get('./api');
+	onHistoryCallback(bars, {noData: false});
 	
-	$.ajax({
-		url: "./api",
-		method: "GET",
-		data: {},
-		beforeSend: function () {
-			
-		},
-	}).done(function (data) {
-		onHistoryCallback(data, {noData: false})
-		/*
-		if (bars.length) {
-			onHistoryCallback(bars, {noData: false})
-		} else {
-			onHistoryCallback(bars, {noData: true})
-		}
-		*/
-	});
+	/* if (bars.length) {
+		onHistoryCallback(bars, {noData: false})
+	} else {
+		onHistoryCallback(bars, {noData: true})
+	} */
 };
 
 
@@ -64,25 +54,32 @@ japi.getBars = function (symbolInfo, resolution, from, to, onHistoryCallback, on
 
 // optional:
 japi.searchSymbols = function (userInput, exchange, symbolType, onResultReadyCallback) {
-	// console.log("searchSymbols()");
+	console.log('searchSymbols()');
+	console.log(userInput, exchange, symbolType, onResultReadyCallback);
 };
 japi.subscribeBars = function (symbolInfo, resolution, onRealtimeCallback, subscriberUID, onResetCacheNeededCallback) {
-	// console.log("subscribeBars()");
+	console.log('subscribeBars()');
+	console.log(symbolInfo, resolution, onRealtimeCallback, subscriberUID, onResetCacheNeededCallback);
 };
 japi.unsubscribeBars = function (subscriberUID) {
-	// console.log("unsubscribeBars()");
+	console.log('unsubscribeBars()');
+	console.log(subscriberUID);
 };
 japi.calculateHistoryDepth = function (resolution, resolutionBack, intervalBack) {
-	// console.log("calculateHistoryDepth()");
+	console.log('calculateHistoryDepth()');
+	// console.log(resolution, resolutionBack, intervalBack);
 };
 japi.getMarks = function (symbolInfo, from, to, onDataCallback, resolution) {
-	// console.log("getMarks()");
+	console.log('getMarks()');
+	console.log(symbolInfo, from, to, onDataCallback, resolution);
 };
 japi.getTimescaleMarks = function (symbolInfo, from, to, onDataCallback, resolution) {
-	// console.log("getTimescaleMarks()");
+	console.log('getTimescaleMarks()');
+	console.log(symbolInfo, from, to, onDataCallback, resolution);
 };
 japi.getServerTime = function (callback) {
-	// console.log("getServerTime()");
+	console.log('getServerTime()');
+	console.log(callback);
 };
 
 
@@ -95,29 +92,30 @@ japi.getServerTime = function (callback) {
 
 
 var widget = new TradingView.widget({
-	symbol: "zob",
+	symbol: 'zob',
 	fullscreen: true,
-	interval: "1D",
-	container_id: "tv_chart_container",
-//	datafeed: new Datafeeds.UDFCompatibleDatafeed("https://demo_feed.tradingview.com"),
+	// width: '70%',
+	interval: '1D',
+	container_id: 'tv_chart_container',
+//	datafeed: new Datafeeds.UDFCompatibleDatafeed('https://demo_feed.tradingview.com'),
 	datafeed: japi,
-	library_path: "lib/tradingview/charting_library/",
+	library_path: 'lib/tradingview/charting_library/',
 	disabled_features: [
-		"header_widget",
-		"left_toolbar",
-		"context_menus", // timezone_menu, scales_context_menu, legend_context_menu, symbol_info, show_chart_property_page
-		"show_chart_property_page",
-//		"remove_library_container_border",
-//		"border_around_the_chart",
-		"edit_buttons_in_legend",
-		"countdown",
-		"display_market_status",
-		"timeframes_toolbar",
-		"go_to_date",
-		"control_bar"
+		'header_widget',
+		'left_toolbar',
+		'context_menus', // timezone_menu, scales_context_menu, legend_context_menu, symbol_info, show_chart_property_page
+		'show_chart_property_page',
+//		'remove_library_container_border',
+//		'border_around_the_chart',
+		'edit_buttons_in_legend',
+		'countdown',
+		'display_market_status',
+		'timeframes_toolbar',
+		'go_to_date',
+		'control_bar'
 	],
 	enabled_features: [
-		"move_logo_to_main_pane"
+		'move_logo_to_main_pane'
 	]
 });
 
