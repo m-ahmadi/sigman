2fdbe5673e0a91b68066a35c3b6ecb3d83204d8f
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-10-10 13:31:39 +0330

    changed Day struct

--- routes/Day.js
+++ routes/Day.js
@@ -1,15 +1,16 @@
 class Day {
 	constructor(_row='') {
 		let row = _row.split(',');
-		if (row.length !== 6) throw new Error('Invalid Day data!');
+		if (row.length !== 7) throw new Error('Invalid Day data!');
 		row = row.map(i => /^[\d\.]+$/.test(i) ? parseInt(i) : i);
 		
 		this.date = row[0];
 		this.open = row[1];
 		this.high = row[2];
 		this.low = row[3];
-		this.close = row[4];
-		this.vol = row[5];
+		this.last = row[4];
+		this.close = row[5];
+		this.vol = row[6];
 		
 		/* this.date = row[0];
 		this.open = row[1];
