c0e1c5b7107ed10b0f85584fa152ff4028024fb7
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-10-10 13:31:12 +0330

    replaced " with '

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-const gulp = require("gulp");
-const livereload = require("gulp-livereload");
-const h = "./views/**/*.hbs";
-const c = "./public/css/**/*.css";
-const j = "./public/js/**/*.js";
+const gulp = require('gulp');
+const livereload = require('gulp-livereload');
+const h = './views/**/*.hbs';
+const c = './public/css/**/*.css';
+const j = './public/js/**/*.js';
 
-gulp.task("live-html", cb => {
+gulp.task('live-html', cb => {
 	gulp.src(h).pipe( livereload() );
 	cb();
 });
-gulp.task("live-css", cb => {
+gulp.task('live-css', cb => {
 	gulp.src(c).pipe( livereload() );
 	cb();
 });
-gulp.task("live-js", cb => {
+gulp.task('live-js', cb => {
 	gulp.src(j).pipe( livereload() );
 	cb();
 });
-gulp.task("live", () => {
+gulp.task('live', () => {
 	livereload.listen();
-	gulp.watch(h, gulp.series("live-html"));
-	gulp.watch(c, gulp.series("live-css"));
-	gulp.watch(j, gulp.series("live-js"));
+	gulp.watch(h, gulp.series('live-html'));
+	gulp.watch(c, gulp.series('live-css'));
+	gulp.watch(j, gulp.series('live-js'));
 });
-gulp.task( "default", gulp.series("live") );
+gulp.task( 'default', gulp.series('live') );
