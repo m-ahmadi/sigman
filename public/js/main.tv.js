3cb370f58a4d318f1432ad465cdd01da189ea158
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-10-20 04:40:21 +0330

    experimenting with jstree

--- public/js/main.tv.js
+++ public/js/main.tv.js
@@ -166,69 +166,106 @@ class Instrument {
 		this.CComVal      = row[14];
 		this.CSecVal      = row[15];
 		this.CSoSecVal    = row[16];
-		this.YVal         = row[17];
+		this.YVal         = parseInt(row[17], 10);
 	}
 }
-const types = {
-	'67':  ['شاخص', 'قیمت'],
-	'68':  ['شاخص', 'اصلی'],
-	'69':  ['شاخص', 'فرابورس'],
-	'70':  ['اوراق مشاركت', 'صکوک اختصاصی 70'],
-	'200': ['اوراق مشاركت', 'انرژی'],
-	'207': ['اوراق مشاركت', 'ارز صادراتی'],
-	'208': ['اوراق مشاركت', 'اوراق صكوك'],
-	'300': ['سهام عادی', 'سهام'],
-	'301': ['اوراق مشاركت', 'اوراق مشارکت'],
-	'303': ['سهام عادی', 'آتیسی'],
-	'304': ['سهام عادی', 'آتی'],
-	'305': ['صندوق سرمايه گذاري', 'صندوق سرمايه گذاري در سهام بورس'],
-	'306': ['اوراق مشاركت', 'آتیسی'],
-	'307': ['سهام عادی', 'تسهیلات فرابورس'],
-	'308': ['اوراق مشاركت', 'کالا'],
-	'309': ['سهام عادی', 'پایه'],
-	'311': ['سهام عادی', 'اختیار خرید'],
-	'312': ['سهام عادی', 'اختیار فروش'],
-	'313': ['سهام عادی', 'شرکتهای کوچک و متوسط'],
-	'315': ['صندوق سرمايه گذاري', 'صندوق سرمایه گذاری قابل معامله انرژی'],
-	'321': ['اختیار فولاد هرمزگان', ''],
-	'322': ['اختیار خ اخزا ( اسناد خزانه داری اسلامی )', '322'],
-	'323': ['اختیارف اخزا ( اسناد خزانه داری اسلامی )', '323'],
-	'400': ['حق تقدم', 'حق تقدم سهم'],
-	'403': ['حق تقدم', 'حق تقدم اتیسی'],
-	'404': ['حق تقدم', 'حق تقدم پایه'],
-	'600': ['اختیار', 'اختیار فروش تبعی'],
-	'601': ['اختیار فروش تبعی ( ذوب آهن اصفهان)', ''],
-	'602': ['اختیار', 'اختیار فروش تبعی فرابورس'],
-	'701': ['کالا', 'گواهی سپرده کالایی'],
-	'706': ['اوراق مشاركت', 'صکوک اختصاصی 706'],
-	'801': ['سلف بورس انرژی', '801'],
-	'802': ['سلف بورس انرژی', '802'],
-	'803': ['سلف بورس انرژی', '803'],
-	'804': ['سلف بورس انرژی', '804'],
-	'901': ['انرژی', '901'],
-	'902': ['انرژی', '902'],
-	'903': ['دارایی فکری', 'دارایی فکری']
-};
+const types = [
+	{ id: 1,   parent: '#', node: 'سهام عادی' },
+	{ id: 300, parent: 1,   node: 'سهام' },
+	{ id: 303, parent: 1,   node: 'آتیسی' },
+	{ id: 309, parent: 1,   node: 'پایه' },
+	{ id: 307, parent: 1,   node: 'تسهیلات فرابورس' },
+	{ id: 313, parent: 1,   node: 'شرکتهای کوچک و متوسط' },
+	{ id: 304, parent: 1,   node: 'آتی' },
+	{ id: 311, parent: 1,   node: 'اختیار خرید' },
+	{ id: 312, parent: 1,   node: 'اختیار فروش' },
+	
+	{ id: 2,   parent: '#', node: 'شاخص' },
+	{ id: 68,  parent: 2,   node: 'شاخص' },
+	{ id: 69,  parent: 2,   node: 'شاخص فرابورس' },
+	{ id: 67,  parent: 2,   node: 'شاخص قیمت' },
+	
+	{ id: 3,   parent: '#', node: 'حق تقدم' },
+	{ id: 400, parent: 3,   node: 'حق تقدم سهم' },
+	{ id: 404, parent: 3,   node: 'حق تقدم پایه' },
+	{ id: 403, parent: 3,   node: 'حق تقدم آتیسی' },
+	
+	{ id: 4,   parent: '#', node: 'اوراق مشاركت'},
+	{ id: 306, parent: 4,   node: 'اوراق مشارکت آتیسی' },
+	{ id: 208, parent: 4,   node: 'اوراق صكوك' },
+	{ id: 706, parent: 4,   node: 'صکوک اختصاصی', alias: [70] },
+	{ id: 200, parent: 4,   node: 'اوراق مشارکت انرژی' },
+	{ id: 207, parent: 4,   node: 'اوراق مشارکت ارز صادراتی' },
+	{ id: 301, parent: 4,   node: 'اوراق مشارکت' },
+	{ id: 308, parent: 4,   node: 'اوراق مشارکت کالا' },
+	
+	{ id: 5,   parent: '#', node: 'صندوق سرمايه گذاري' },
+	{ id: 305, parent: 5,   node: 'صندوق سرمايه گذاري در سهام بورس' },
+	{ id: 315, parent: 5,   node: 'صندوق سرمایه گذاری قابل معامله انرژی' },
+	
+	{ id: 6,   parent: '#', node: 'اختیار' },
+	{ id: 322, parent: 6,   node: 'اختیار خ اخزا (اسناد خزانه داری اسلامی)' },
+	{ id: 323, parent: 6,   node: 'اختیارف اخزا (اسناد خزانه داری اسلامی)' },
+	{ id: 321, parent: 6,   node: 'اختیار فولاد هرمزگان' },
+	{ id: 601, parent: 6,   node: 'اختیار فروش تبعی (ذوب آهن اصفهان)' },
+	{ id: 600, parent: 6,   node: 'اختیار فروش تبعی' },
+	{ id: 602, parent: 6,   node: 'اختیار فروش تبعی فرابورس' },
+	
+	{ id: 903, parent: '#', node: 'دارایی فکری' },
+	{ id: 701, parent: '#', node: 'گواهی سپرده کالایی' },
+	{ id: 901, parent: '#', node: 'انرژی', alias: [902] },
+	{ id: 801, parent: '#', node: 'سلف بورس انرژی', alias: [802,803,804] }
+];
 
-const uniq = [];
-let dd;
 async function test() {
-	
 	let ins = await $.get('instruments.csv');
-	
 	ins = ins.split('\n').map(i => new Instrument(i));
-	ins.forEach(i => uniq.indexOf(i.YVal) === -1 ? uniq.push(i.YVal) : undefined);
-	ins.forEach(i => uniq.indexOf(i.YVal) === -1 ? uniq.push(i.YVal) : undefined);
-	// Object.keys(types).forEach(k => types[k][2] = 0);
-	// ins.forEach(i => types[i.YVal] ? types[i.YVal][2] += 1 : undefined);
 	
-	const l1 = [...new Set(Object.keys(types).map(i => types[i][0]))].map((v,i)=>({id: i+1, node: v, parent: '#'}));
-	const l2 = Object.keys(types).map((k,i) => ({id: l1.length+i+1, node: types[k][1], parent: l1.find(j=>j.node===types[k][0]).id}));
-	dd = l1.concat(l2);
-	dd = dd.filter(i => i.node);
+	types.forEach(i => i.count = 0);
+	ins.forEach(i => {
+		const idx = types.findIndex( j => j.id === i.YVal || (j.alias && j.alias.includes(i.YVal)) );
+		if (idx !== -1) types[idx].count += 1;
+	});
 	
-	jd = dd.map(i => ({ id: ''+i.id, text: i.node, parent: ''+i.parent, state: {opened: true}, ...i.parent !== '#' && {icon: 'jstree-file'} }));
-	jd.filter(i=> i.parent === '#' && !jd.filter(j=>j.parent===i.id).length).forEach(i => i.icon = 'jstree-file');
+	// count of categories
+	dd = types.map(i => {
+		let count = i.count;
+		if (i.parent === '#') {
+			const children = types.filter(j => j.parent === i.id);
+			if (children.length) count = children.map(i=>i.count).reduce((a,c)=>a+c);
+		}
+		return Object.assign(i, { count });
+	});
+	// remove 0 counts
+	dd = dd.filter(i => i.count !== 0);
+	
+	// merge 1-child categories:
+	dd
+		.filter(i => i.parent === '#' && dd.filter(j=>j.parent===i.id).length === 1)
+		.map( i=> dd.findIndex(j=>j.id===i.id) )
+		.forEach(i => {
+			dd.find(j => j.parent === dd[i].id).parent = '#';
+			dd.splice(i, 1);
+		});
+	// put child-less root-nodes at end:
+	const childless = dd.filter(i => i.parent === '#' && !dd.filter(j=>j.parent===i.id).length);
+	childless.map( i => dd.splice(dd.findIndex(j=>j.id===i.id), 1) );
+	dd = dd.concat(childless);
+	
+	// types.filter((v,i,a) => v.parent === '#' && a.find(j=>j.parent === v.id) ) // categories
+	// types.filter((v,i,a) => !a.find(j=>j.parent === v.id) ) // not category
+	
+	jd = dd.map(i => {
+		return {
+			id: ''+i.id,
+			text: i.node + ` <small style="color:grey;">(${i.count})</small>`,
+			parent: ''+i.parent,
+			// state: { opened: true },
+			...i.id === 300 && {state: { selected: true }},
+			...i.parent !== '#' && {icon: 'jstree-file'}
+		};
+	});
+	jd.filter(i => i.parent === '#' && !jd.filter(j=>j.parent===i.id).length).forEach(i => i.icon = 'jstree-file');
 	
 	$('#tree-container').jstree({
 		core: { data: jd },
@@ -236,7 +273,7 @@ async function test() {
 		types: { file: { icon: 'jstree-icon jstree-file', valid_children: ['none'] } }
 	});
 	
-	var sd = dd.map((v,i,a) => {
+	var sd = types.map((v,i,a) => {
 		if (v.parent === '#') return { text: v.node, children: a.filter(j=>j.parent === v.id).map(i=>i.node) }
 	}).filter(i=>i); 
 	
