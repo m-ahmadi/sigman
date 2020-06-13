aa8e7e3f57ecc74f71d174120f9cb07d12f684ea
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-06-13 19:11:30 +0430

    added sigman/datable/combo
    added jstree as dependency

--- src/sigman/datable/datable.js
+++ src/sigman/datable/datable.js
@@ -1,6 +1,7 @@
 import jalaali from 'jalaali-js';
 
 import htm from './datable.htm';
+import combo from './combo/combo.js';
 
 let $$;
 const dateRgx = /^[0189]{1}\d{1}\/(1[0-2]|[1-9])\/([1-9]|[1-2]\d|3[01])$/;
@@ -11,6 +12,9 @@ export default Object.assign(inst, {create});
 async function create($root) {
   $root.html(htm);
   $$ = __els('[data-root="datable"]');
+  
+  if (!combo.inited) combo.init();
+  await combo.create($$.combo);
   
   $$.date.on('input blur change', function () {
     const v = +this.value;
