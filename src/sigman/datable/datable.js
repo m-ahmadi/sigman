6096c56788542725e50a966ec57cba5a01b6814b
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-06-13 18:45:24 +0430

    added sigman/datable

---
+++ src/sigman/datable/datable.js
@@ -0,0 +1,37 @@
+import jalaali from 'jalaali-js';
+
+import htm from './datable.htm';
+
+let $$;
+const dateRgx = /^[0189]{1}\d{1}\/(1[0-2]|[1-9])\/([1-9]|[1-2]\d|3[01])$/;
+
+const inst = new EventTarget();
+export default Object.assign(inst, {create});
+
+async function create($root) {
+  $root.html(htm);
+  $$ = __els('[data-root="datable"]');
+  
+  $$.date.on('input blur change', function () {
+    const v = +this.value;
+    this.value =
+      v > this.max ? this.max :
+      v < this.min ? this.min : v;
+  });
+  
+  // TODO: place today's date in date inputs
+  const jDate = jalaali.toJalaali(new Date());
+  
+  // TODO: increment year input like 01 02 ... 09 10
+  // TODO: make day,month input jump to next one on keydown
+  $$.date.on('wheel', function (e) {
+    const { deltaY } = e.originalEvent;
+    const v = +this.value;
+    this.value =
+      deltaY < 0 ? v + 1 :
+      deltaY > 0 ? v - 1 : this.value;
+    $(this).trigger('change');
+  });
+  
+}
+
--- src/sigman/sigman.js
+++ src/sigman/sigman.js
@@ -1,12 +1,16 @@
 import './sigman.scss';
 import htm from './sigman.htm';
 
+import datable from './datable/datable.js';
+
 let $$;
 
 export default function init() {
   __$routerOutlet.html(htm);
   
   $$ = __els('[data-root="sigman"]');
+  
+  datable.create($$.root);
 
   /* $$.showUs.on('click', function () {
     $$.root.append( $(article) );
